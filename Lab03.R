1b0a677400c711e754399fa9edf6bec1b9182327
melissayauri <yaurimelissa@gmail.ccom>
2023-05-03 08:27:37 -0500

    añadiendo escala y colores a la gráfica scatter

--- Lab03.R
+++ Lab03.R
@@ -28,7 +28,7 @@ View(http_data)
 # Creando nueva tabla segun las repeticiones de las direcciones, para obtener direcciones únicas
 directions_table <- data.frame(Directions = http_data$Directions, Response_code =http_data$Response_code) 
 concurrences <- as.data.frame(table(directions_table))
-View(concurrences)
+
 # Filtrando los valores existentes y ordenando de forma ascendente por la columna Response_code
 # 200, 302, 304, 400, 403, 404, 500, 501
 directions_data <- filter(concurrences, Freq > 0) 
@@ -36,7 +36,6 @@ directions_data <- filter(concurrences, Freq > 0)
 
 directions_data <- directions_data %>%
   arrange(Response_code)
-View(directions_data)
 
 unique_codes <- unique(directions_data$Response_code)
 # 200, 302, 304, 400, 403, 404, 500, 501
@@ -70,31 +69,25 @@ code501_users
 # contar la frecuencia de la columna http
 freq_http <- table(http_data$Method)
 method_data <- data.frame(http = names(freq_http), freq_http = as.vector(freq_http))
-View(method_data)
-# "GET     46020
-# "HEAD       106
-# "POST      1622
+
 
 # Hallando la frecuencia de la columna http, filtrando previamente los recursos tipo imagen
 different_image_data <- http_data %>%
   filter(!grepl("(?i)\\.(gif|jpg|jpeg|png|bmp)$", Resource))
-View(different_image_data)
 
 freq_http2 <- table(different_image_data$Method)
 method2_data <- data.frame(http = names(freq_http2), freq_http2 = as.vector(freq_http2))
-View(method2_data)
-#  http freq_http
-#  "GET     46020
-# "HEAD       106
-# "POST      1622
+
 
 ### Pregunta 4
-# Eligan el grafico que les convenga
+
 tabla_frecuencia <-table(http_data$Response_code)
 response_code_table <- data.frame(Response_code = names(tabla_frecuencia), 
                           Frecuencia = as.vector(tabla_frecuencia))
 
-
+# Argumentando
+# Estos tipos de gráficas permiten visualizar la frecuencia de las distintas categorías presentes en una variable, 
+# lo que puede ayudar a identificar patrones y tendencias.
 ggplot(response_code_table, aes(x = Response_code, y = Frecuencia)) +
   geom_bar(stat = "identity") +
   labs(title = "Gráfico 2 de Respuesta de Código",
@@ -121,7 +114,7 @@ epa_http_one_hot <- one_hot(as.data.table(http_data_filtered), sparsifyNAs = TRU
 
 http_data$Resource_size <- nchar(http_data$Resource)
 
-# Agrupamiento de 4 y 3
+# Agrupamiento de 4 y 3, esto se puede cambiar por otros valores como 5, 6, 7, etc
 
 results2 <- kmeans(epa_http_one_hot, centers = 4)
 
@@ -130,18 +123,33 @@ results3 <- kmeans(epa_http_one_hot, centers = 3)
 # Pregunta 6
 
 # Gráficas en base a la columna bytes y Resource_size segun el tipo de agrupamiento
-colores2 <- rainbow(n = length(unique(results2$cluster)))
+# Solo para que los resultados sean reproducibles y no aleatorios
+set.seed(123) # si no se coloca la grafica cambia en cada ejecución
+
+## Gráfica con cluster 4
+# Solo usar si quieres colores aleatorios
+#colores2 <- rainbow(n = length(unique(results2$cluster)))
+colores2 <- c("#f06b50", "#8cbfaf", "#fca699", "#91204d")
 grap1 <- plot(x = http_data$Bytes, y = http_data$Resource_size, col = colores2[results2$cluster], main="GRafico con 2")
-grap1
+# solo usar esta opcion si quieren cambiar la escala de notación cientifica a númerica
+options(scipen = 999)
 # Creando leyenda
 legend("topright", legend = levels(factor(results2$cluster)), col = colores2, pch = 16)
 
-colores3 <- rainbow(n = length(unique(results3$cluster)))
+### Gráfica con Cluster 3
+
+#colores3 <- rainbow(n = length(unique(results3$cluster)))
+colores3 <- c("#ad2bad","#00988d", "#dbbf6b")
 grap2 <- plot(x = http_data$Bytes, y = http_data$Resource_size, col = colores3[results3$cluster], main="GRafico con 3")
-grap2
+# solo usar esta opcion si quieren cambiar la escala de notación cientifica a númerica
+options(scipen = 999)
 # Creando leyenda
 legend("topright", legend = levels(factor(results3$cluster)), col = colores3, pch = 16)
 
 #termino
 
 
+# La interpretación de cada gráfica es en base a la cantidad de puntos según el
+# el tipo de cluster
+# Por ejemplo en la gráfica 2, se visualiza que existe más presencia de cluster 3 
+# En el gráfico 2, se aprecia que hay más cantidad de cluster 2
